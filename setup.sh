2a802f06a68c419bbecddd87e778b351bcdb951d
jubetz <44002142+jubetz@users.noreply.github.com>
2019-03-29 16:21:46 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,3 +11,17 @@ vagrant plugin install vagrant-libvirt
 vagrant plugin install vagrant-mutate
 #
 # add the work here for name storage mount and use by qemu/libvirt
+parted -a optimal /dev/nvme0n1 mklabel gpt
+parted -a optimal /dev/nvme0n1 mkpart primary ext4 0% 100%
+mkfs.ext4 /dev/nvme0n1p1
+mount /dev/nvme0n1p1 /mnt/nvme -t ext4
+virsh pool-destroy default
+virsh pool-undefine default
+mkdir /mnt/nvme/.libvirt
+virsh pool-define-as --name default --type dir --target /mnt/nvme/.libvirt
+virsh pool-autostart default
+virsh pool-start default
+mkdir .vagrant_boxes
+export VAGRANT_HOME=/mnt/nvme/.vagrant_boxes
+vagrant plugin install vagrant-libvirt
+
