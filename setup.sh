f2c55e4488ad066733033da46f34a9d11758f234
jubetz <44002142+jubetz@users.noreply.github.com>
2019-03-29 16:00:58 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ apt-get update -y
 apt-get install libvirt-bin libvirt-dev qemu-utils qemu
 /etc/init.d/libvirt-bin restart
 addgroup libvirtd
-usermod -a -G libvirtd USERNAME
+usermod -a -G libvirtd root
 wget https://releases.hashicorp.com/vagrant/2.2.4/vagrant_2.2.4_x86_64.deb
 dpkg -i https://releases.hashicorp.com/vagrant/2.2.4/vagrant_2.2.4_x86_64.deb
 vagrant plugin install vagrant-libvirt
