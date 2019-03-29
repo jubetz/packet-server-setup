4ca1fac3ab4668be633e218e4c4b6092cde9af1b
jubetz <44002142+jubetz@users.noreply.github.com>
2019-03-29 16:00:27 -0700

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+apt-get update -y
+apt-get install libvirt-bin libvirt-dev qemu-utils qemu
+/etc/init.d/libvirt-bin restart
+addgroup libvirtd
+usermod -a -G libvirtd USERNAME
+wget https://releases.hashicorp.com/vagrant/2.2.4/vagrant_2.2.4_x86_64.deb
+dpkg -i https://releases.hashicorp.com/vagrant/2.2.4/vagrant_2.2.4_x86_64.deb
+vagrant plugin install vagrant-libvirt
+vagrant plugin install vagrant-mutate
+#
+# add the work here for name storage mount and use by qemu/libvirt
